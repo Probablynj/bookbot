a9cdd374a0e549c1933822236d47be394295f9e3
probablynj <slcpunk87@gmail.com>
2025-03-03 11:56:33 -0500

    5 ray wont stfu

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from stats import accept_text
 from stats import get_character_num
+from stats import sort_characters
+
+def print_report(filepath, word_count, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("-------- Character Count --------")
+
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        if char.isalpha():
+            print(f"{char}: {char_dict['count']}")
+
+    print("============== END ==============")
 
 def main():
     result = accept_text()
+    word_count = len(result.split())
+    report = sort_characters()
     letters_result = get_character_num(result)
-    print(f"{result} words found in the document")
+    printReport = print_report(result, word_count, char_count_dict)
+    
+    print(f"{word_count} words found in the document")
     print(letters_result)
+    print(printReport)
+    
+
     
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,6 @@
 def accept_text():
     text = get_book_text("books/frankenstein.txt")
-    words = text.split()
-    amount = len(words)
-    return amount
+    return text
 
 def get_book_text(f):
     with open(f) as f:
@@ -14,3 +12,29 @@ def get_character_num(text):
     for letters in text:
         characters[letters.lower()] = characters.get(letters.lower(), 0) + 1
     return characters
+
+def sort_characters(char_count_dict):
+    chars_list = []
+
+    for char, count in char_count_dict.item():
+        chars_list.append({"char": char, "count": count})
+
+    def sort_on(dict):
+        return dict["count"]
+    
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
+
+"""def print_report(filepath, word_count, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("-------- Character Count --------")
+
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        if char.isalpha():
+            print(f"{char}: {char_dict['count']}")
+
+    print("============== END ==============")"""
